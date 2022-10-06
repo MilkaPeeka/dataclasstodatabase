6184965d811744a1229dd436b10de46bff77c644
yuval hadar <yuvalhad12@gmail.com>
2022-10-06 18:47:45 +0300

    some internal testing

--- main.py
+++ main.py
@@ -16,7 +16,15 @@ if __name__ == "__main__":
     test_obj2 = Test("Roei", 1, False)
     test_obj3 = Test("Daniel", 19, True)
 
-    # test_db.insert(test_obj3,test_obj1,test_obj2)
-
+    test_db.insert(test_obj3, test_obj, test_obj2)
+    # now db contains all three objects
     pr = test_db.fetch_all()
+    # will contain a list of objects
+
     test_db.delete(test_obj)
+    # now db contains all objects except of first one
+
+    test_obj2.age = 14
+    test_obj2.is_adult = True
+    test_db.update("name", test_obj2)
+    # now test_obj2 is updated to new values
