0502268c795a7138282e1ecb517a54ad10312dcc
yuval hadar <yuvalhad12@gmail.com>
2022-12-30 21:31:42 +0200

    working

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ class backAccounts:
 back_accounts_db = dctodb(backAccounts, "Test.db")
 
 bank_account = backAccounts("Yuval", [BankPair("mainBank", 1243333), BankPair("secondAccount", 9938234), BankPair("secretBank", -9999)])
+bank_account2 = backAccounts("Eyal", [BankPair("IDFBank", 8873453492), BankPair("MainBank", 93453859), BankPair("FraudBank", -9999)])
 
-# back_accounts_db.insert_one(bank_account)
+# back_accounts_db.insert_one(bank_account2)
 
-print(back_accounts_db.fetch_where('id == 1'))
+print(back_accounts_db.fetch_where('id == 2'))
 
 
 # @dataclass 
